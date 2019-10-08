e85f79aace27169e25e35ebfd2ede1d85f4e3ecb
YTMartian <1773741250@qq.com>
2019-10-08 22:45:38 +0800

    create data tables

--- Server/system/admin.py
+++ Server/system/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
-from .models import Test
+from .models import User, History, UserPlate
 
 
-class TestAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'num')
-    list_display = ('id', 'num')
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'password')
+    list_display = ('name', 'password')
     list_per_page = 20
-    search_fields = ('id', 'num')
+    search_fields = ('name', 'password')
 
 
-admin.site.register(Test, TestAdmin)
+class HistoryAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'license_plate')
+    list_display = ('name', 'time', 'photograph', 'license_plate', 'type', 'state', 'price')
+    list_per_page = 20
+    search_fields = ('name', 'license_plate')
+
+
+class UserPlateAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'license_plate')
+    list_display = ('name', 'license_plate')
+    list_per_page = 20
+    search_fields = ('name', 'license_plate')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(History, HistoryAdmin)
+admin.site.register(UserPlate, UserPlateAdmin)
--- Server/system/models.py
+++ Server/system/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 
 
-class Test(models.Model):
-    id = models.CharField('id', max_length = 20, blank = False, primary_key = True)
-    num = models.CharField('值', max_length = 100, blank = False)
+class User(models.Model):
+    name = models.CharField('用户名', max_length = 20, blank = False)
+    password = models.CharField('密码', max_length = 100, blank = False)
     
     def __str__(self):
-        return self.id
+        return self.name
     
     class Meta:
-        ordering = ['-id']
+        ordering = ['-name']
+
+
+class History(models.Model):
+    name = models.CharField('用户名', max_length = 20, blank = False)
+    time = models.DateTimeField('时间', auto_now = False, blank = False)
+    photograph = models.CharField('图片', max_length = 100, blank = False)
+    license_plate = models.CharField('车牌号', max_length = 100, blank = False)
+    type = models.CharField('车型', max_length = 10, blank = False)
+    state = models.BooleanField('状态', default = False, blank = False)  # True代表离开，收费字段填入金额
+    price = models.FloatField('收费', blank = True, default = 0.0)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-name']
+
+
+class UserPlate(models.Model):
+    name = models.CharField('用户名', max_length = 20, blank = False)
+    license_plate = models.CharField('车牌号', max_length = 100, blank = False)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-name']
