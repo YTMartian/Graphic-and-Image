963c83c5839d3e79a5278287a2906509f748ca26
YTMartian <1773741250@qq.com>
2019-10-10 21:08:27 +0800

    app popup window

--- Client/app/src/main/java/com/example/carlicense/MainActivity.java
+++ Client/app/src/main/java/com/example/carlicense/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.carlicense;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -20,7 +23,10 @@ import com.github.clans.fab.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    boolean is_get_in=false;
+    boolean is_get_in = false;
+    Dialog myDialog;
+    String server_ip;
+    String username;
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -46,6 +52,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationUI.setupWithNavController(navigationView, navController);
         //设置监听导航按钮的点击
         navigationView.setNavigationItemSelectedListener(this);
+
+        myDialog = new Dialog(this);
+        server_ip = "192.168.137.1";
     }
 
     @Override
@@ -93,15 +102,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     //点击get_in按钮
     public void clickGetIn(View view) {
         FloatingActionButton button = findViewById(R.id.get_in);
+        //弹出确认框
+        myDialog.setContentView(R.layout.popup);
+        //注意应在myDialog下find，否则就是空指针错误
+        MyImageView license_plate_image = myDialog.findViewById(R.id.license_plate_image);
+        TextView license_plate_text = myDialog.findViewById(R.id.license_plate_text);
+        TextView car_type = myDialog.findViewById(R.id.car_type);
+        TextView now_time = myDialog.findViewById(R.id.now_time);
+        TextView pay_toll = myDialog.findViewById(R.id.pay_toll);
         //更改按钮图标和文字
         if (is_get_in) {
             button.setImageResource(R.drawable.ic_get_in);
             button.setLabelText("进入");
+            pay_toll.setVisibility(View.VISIBLE);
+            myDialog.show();
+            license_plate_image.setImageURL("http://www.potatochip.cn/wallpaper-118.jpg");
         } else {
             button.setImageResource(R.drawable.ic_get_out);
             button.setLabelText("离开");
+            pay_toll.setVisibility(View.GONE);
+            myDialog.show();
+            license_plate_image.setImageURL("http://www.potatochip.cn/wallpaper-112.jpg");
         }
         is_get_in = !is_get_in;
+    }
+
+    //点击确定按钮
+    public void clickEnsure(View view) {
+        myDialog.dismiss();
+    }
+
+    //点击更改ip按钮
+    public void clickChangeIp(View view) {
+        EditText ip = findViewById(R.id.ip_address);
+        if (ip.getText().toString().isEmpty()) return;
+        server_ip = ip.getText().toString();
+        myDialog.setContentView(R.layout.popup_ip_ensure);
+        TextView ip_address_hint = myDialog.findViewById(R.id.ip_address_hint);
+        ip_address_hint.setText(ip_address_hint.getText() + server_ip);
+        myDialog.show();
     }
 
 }
